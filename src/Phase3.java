99a4ce5fd8f17eba49893e0b8613e56daf25a6eb
jasonpettit <46112312+jasonpettit@users.noreply.github.com>
2019-06-01 16:18:11 -0700

    Phase 3 start

--- src/Phase3.java
+++ src/Phase3.java
@@ -5,12 +5,18 @@
  * EXERCISE: Module 5 GUI Cards - Phase 2
  */
 
+/* Assignments:
+ * CARDTABLE: Jason / Marcus
+ * GUICARD: Colin / Sergio
+ * CLEANUP EXISTING CARD / HAND / DECK: Adam
+*/
+
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.util.Random;
 
-public class Phase3
+public class Phase2
 {
    static int NUM_CARDS_PER_HAND = 7;
    static int NUM_PLAYERS = 2;
@@ -23,20 +29,7 @@ public class Phase3
    {
       int k;
       Icon tempIcon;
-      
-      //phase 3 variables
-      int numPacksPerDeck = 1;
-      int numJokersPerPack = 2;
-      int numUnusedCardsPerPack = 0;
-      Card[] unusedCardsPerPack = null;
 
-      CardGameFramework highCardGame = new CardGameFramework( 
-            numPacksPerDeck, numJokersPerPack,  
-            numUnusedCardsPerPack, unusedCardsPerPack, 
-            NUM_PLAYERS, NUM_CARDS_PER_HAND);
-      
-      highCardGame.deal();
-      
       // establish main frame in which program will run
       CardTable myCardTable
          = new CardTable("CardTable", NUM_CARDS_PER_HAND, NUM_PLAYERS);
@@ -229,7 +222,7 @@ class GUICard
 //START class Card
 /**
  * The Card class allows for the representation and manipulation of a single
- * playing card as found in a standard 52 card deck.
+ * playing card as found in a standard 56 card deck.
  */
 class Card
 {
@@ -435,7 +428,7 @@ class Card
  */
 class Hand
 {
-   public static final int MAX_CARDS = 52;
+   public static final int MAX_CARDS = 56;
 
    private Card[] myCards;
    private int numCards;
@@ -443,7 +436,7 @@ class Hand
    //Default constructor
    /**
     * There is a default Hand() constructor that creates an empty hand.  A hand
-    * can hold no more than 52 cards, and the number of cards in the hand after
+    * can hold no more than 56 cards, and the number of cards in the hand after
     * construction is 0.
     */
    public Hand()
@@ -463,7 +456,7 @@ class Hand
    /**
     * public boolean takeCard(Card card) puts a new card in the players hand
     * and also checks that taking a new card would not violate the MAX_CARDS
-    * allowed in the hand, which is set to MAX_CARDS = 52 by the Hand constructor.
+    * allowed in the hand, which is set to MAX_CARDS = 56 by the Hand constructor.
     * @param card
     * @return true or false
     */
@@ -621,16 +614,16 @@ class Deck
    */
    public void init(int numPacks)
    {
-      cards = new Card[numPacks * 52];
+      cards = new Card[numPacks * 56];
       for (int i = 0; i < numPacks; i++)
       {
          System.arraycopy(masterPack, 0, this.cards, i * masterPack.length,
             masterPack.length);
       }
-      if ((numPacks * 52) > MAX_CARDS)
+      if ((numPacks * 56) > MAX_CARDS)
          topCard = MAX_CARDS - 1;
       else
-         topCard = (numPacks * 52) - 1;
+         topCard = (numPacks * 56) - 1;
    }
    /**
    * shuffle method
@@ -776,172 +769,3 @@ class Deck
    }
 }
 //END class Deck
-
-//class CardGameFramework  ----------------------------------------------------
-class CardGameFramework
-{
- private static final int MAX_PLAYERS = 50;
-
- private int numPlayers;
- private int numPacks;            // # standard 52-card packs per deck
-                                  // ignoring jokers or unused cards
- private int numJokersPerPack;    // if 2 per pack & 3 packs per deck, get 6
- private int numUnusedCardsPerPack;  // # cards removed from each pack
- private int numCardsPerHand;        // # cards to deal each player
- private Deck deck;               // holds the initial full deck and gets
-                                  // smaller (usually) during play
- private Hand[] hand;             // one Hand for each player
- private Card[] unusedCardsPerPack;   // an array holding the cards not used
-                                      // in the game.  e.g. pinochle does not
-                                      // use cards 2-8 of any suit
-
- public CardGameFramework( int numPacks, int numJokersPerPack,
-       int numUnusedCardsPerPack,  Card[] unusedCardsPerPack,
-       int numPlayers, int numCardsPerHand)
- {
-    int k;
-
-    // filter bad values
-    if (numPacks < 1 || numPacks > 6)
-       numPacks = 1;
-    if (numJokersPerPack < 0 || numJokersPerPack > 4)
-       numJokersPerPack = 0;
-    if (numUnusedCardsPerPack < 0 || numUnusedCardsPerPack > 50) //  > 1 card
-       numUnusedCardsPerPack = 0;
-    if (numPlayers < 1 || numPlayers > MAX_PLAYERS)
-       numPlayers = 4;
-    // one of many ways to assure at least one full deal to all players
-    if  (numCardsPerHand < 1 ||
-          numCardsPerHand >  numPacks * (52 - numUnusedCardsPerPack)
-          / numPlayers )
-       numCardsPerHand = numPacks * (52 - numUnusedCardsPerPack) / numPlayers;
-
-    // allocate
-    this.unusedCardsPerPack = new Card[numUnusedCardsPerPack];
-    this.hand = new Hand[numPlayers];
-    for (k = 0; k < numPlayers; k++)
-       this.hand[k] = new Hand();
-    deck = new Deck(numPacks);
-
-    // assign to members
-    this.numPacks = numPacks;
-    this.numJokersPerPack = numJokersPerPack;
-    this.numUnusedCardsPerPack = numUnusedCardsPerPack;
-    this.numPlayers = numPlayers;
-    this.numCardsPerHand = numCardsPerHand;
-    for (k = 0; k < numUnusedCardsPerPack; k++)
-       this.unusedCardsPerPack[k] = unusedCardsPerPack[k];
-
-    // prepare deck and shuffle
-    newGame();
- }
-
- // constructor overload/default for game like bridge
- public CardGameFramework()
- {
-    this(1, 0, 0, null, 4, 13);
- }
-
- public Hand getHand(int k)
- {
-    // hands start from 0 like arrays
-
-    // on error return automatic empty hand
-    if (k < 0 || k >= numPlayers)
-       return new Hand();
-
-    return hand[k];
- }
-
- public Card getCardFromDeck() { return deck.dealCard(); }
-
- public int getNumCardsRemainingInDeck() { return deck.getNumCards(); }
-
- public void newGame()
- {
-    int k, j;
-
-    // clear the hands
-    for (k = 0; k < numPlayers; k++)
-       hand[k].resetHand();
-
-    // restock the deck
-    deck.init(numPacks);
-
-    // remove unused cards
-    for (k = 0; k < numUnusedCardsPerPack; k++)
-       deck.removeCard( unusedCardsPerPack[k] );
-
-    // add jokers
-    for (k = 0; k < numPacks; k++)
-       for ( j = 0; j < numJokersPerPack; j++)
-          deck.addCard( new Card('X', Card.Suit.values()[j]) );
-
-    // shuffle the cards
-    deck.shuffle();
- }
-
- public boolean deal()
- {
-    // returns false if not enough cards, but deals what it can
-    int k, j;
-    boolean enoughCards;
-
-    // clear all hands
-    for (j = 0; j < numPlayers; j++)
-       hand[j].resetHand();
-
-    enoughCards = true;
-    for (k = 0; k < numCardsPerHand && enoughCards ; k++)
-    {
-       for (j = 0; j < numPlayers; j++)
-          if (deck.getNumCards() > 0)
-             hand[j].takeCard( deck.dealCard() );
-          else
-          {
-             enoughCards = false;
-             break;
-          }
-    }
-
-    return enoughCards;
- }
-
- void sortHands()
- {
-    int k;
-
-    for (k = 0; k < numPlayers; k++)
-       hand[k].sort();
- }
-
- Card playCard(int playerIndex, int cardIndex)
- {
-    // returns bad card if either argument is bad
-    if (playerIndex < 0 ||  playerIndex > numPlayers - 1 ||
-        cardIndex < 0 || cardIndex > numCardsPerHand - 1)
-    {
-       //Creates a card that does not work
-       return new Card('M', Card.Suit.spades);      
-    }
- 
-    // return the card played
-    return hand[playerIndex].playCard(cardIndex);
- 
- }
-
-
- boolean takeCard(int playerIndex)
- {
-    // returns false if either argument is bad
-    if (playerIndex < 0 || playerIndex > numPlayers - 1)
-       return false;
-   
-     // Are there enough Cards?
-     if (deck.getNumCards() <= 0)
-        return false;
-
-     return hand[playerIndex].takeCard(deck.dealCard());
- }
-
-}
